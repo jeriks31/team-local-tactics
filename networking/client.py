598ec006cc92fd6d95561f2abda35bb0a57fd338
Jan Erik Syltøy <jan_erik00@hotmail.com>
2022-03-10 21:02:12 +0100

    Copy necessary functions from team-local-tactics.py to client.py

--- networking/client.py
+++ networking/client.py
@@ -1,4 +1,8 @@
 from collections import Callable
+from rich import print
+from rich.prompt import Prompt
+from rich.table import Table
+from core import Champion
 import common
 import socket
 
@@ -40,3 +44,40 @@ def sendMessage(message: str, args: list[str]) -> str:
     _connection.send(request.encode("utf8"))
     response = _connection.recv(common.MSG_SIZE).decode("utf8")
     return response
+
+
+def print_available_champs(champions: dict[str, Champion]) -> None:
+
+    # Create a table containing available champions
+    available_champs = Table(title='Available champions')
+
+    # Add the columns Name, probability of rock, probability of paper and
+    # probability of scissors
+    available_champs.add_column("Name", style="cyan", no_wrap=True)
+    available_champs.add_column("prob(:raised_fist-emoji:)", justify="center")
+    available_champs.add_column("prob(:raised_hand-emoji:)", justify="center")
+    available_champs.add_column("prob(:victory_hand-emoji:)", justify="center")
+
+    # Populate the table
+    for champion in champions.values():
+        available_champs.add_row(*champion.str_tuple)
+
+    print(available_champs)
+
+
+def input_champion(prompt: str,
+                   color: str,
+                   champions: dict[str, Champion],
+                   player1: list[str],
+                   player2: list[str]) -> None:
+
+    # Prompt the player to choose a champion and provide the reason why
+    # certain champion cannot be selected
+    while True:
+        name = Prompt.ask(f'[{color}]{prompt}')
+        if name not in champions: print(f'The champion {name} is not available. Try again.')
+        elif name in player1: print(f'{name} is already in your team. Try again.')
+        elif name in player2: print(f'{name} is in the enemy team. Try again.')
+        else:
+            player1.append(name)
+            break
