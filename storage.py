8322aaf3f589f095a2f0fbf1ed7cad17d9f99870
Jan Erik Syltøy <jan_erik00@hotmail.com>
2022-03-10 19:16:37 +0100

    Fixed file path misinterpreted as escape sequence

--- storage.py
+++ storage.py
@@ -10,7 +10,7 @@ def _parse_match(matchText: str) -> tuple[str,str,int,int]:
 
 def load_champions() -> dict[str, Champion]:
     champions = {}
-    with open('storage\champions.csv', 'r') as file:
+    with open('storage\\champions.csv', 'r') as file:
         for championText in file.readlines():
             champion = _parse_champion(championText)
             champions[champion.name] = champion
@@ -18,14 +18,14 @@ def load_champions() -> dict[str, Champion]:
 
 def load_matches() -> list[tuple[str,str,float,float]]:
     matches = []
-    with open('storage\matches.csv', 'r') as file:
+    with open('storage\\matches.csv', 'r') as file:
         for matchText in file.readlines():
             match = _parse_match(matchText)
             matches.append(match)
     return matches
 
 def save_match(match:Match) -> None:
-    with open('storage\matches.csv', 'a') as file:
+    with open('storage\\matches.csv', 'a') as file:
         red_name = match.red_team.name if len(match.red_team.name) > 0 else 'Red'
         blue_name = match.blue_team.name if len(match.blue_team.name) > 0 else 'Blue'
         red_score, blue_score = match.score()
